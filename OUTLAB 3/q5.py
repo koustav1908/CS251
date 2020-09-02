4aa41c27b625be8e969222bbab80332739ba0ee6
Koustav Sen <koustavsen17@gmail.com>
2020-09-02 17:45:01 +0530

    Added q5.py

--- OUTLAB 3/q5.py
+++ OUTLAB 3/q5.py
@@ -1,1 +1,26 @@
+def rotate(m):
+	n=len(m)
+	matrix=[]
+	for i in range(0,n):
+		j=n-1
+		a=[]
+		while j>=0:
+			a.append(m[j][i])
+			j=j-1;
+		matrix.append(a)
+	return matrix
+l=1
+matrix=[]
+i=0
+while i < l:
+	a=list(map(int, input().split()))
+	matrix.append(a);
+	l=len(a)
+	i=i+1
 
+matrix_90=rotate(matrix)
+
+for i in range(0,l):
+	for j in range(0,l):
+		print(matrix_90[i][j], end=" ")
+	print()
